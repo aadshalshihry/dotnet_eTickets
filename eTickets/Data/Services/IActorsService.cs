bdab72ff7ade92fcd8ec49a667a95edebaa13040
Roman Alshehri <aa.alshehri@artaldigital.com>
2023-03-21 15:11:10 +0300

    Add Repository Pattern

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,14 +1,9 @@
 
 
+using eTickets.Data.Base;
 using eTickets.Models;
 
 namespace eTickets.Data.Services;
 
-public interface IActorsService
-{
-    Task<IEnumerable<Actor>> GetAllAsync();
-    Task<Actor> GetByIdAsync(int id);
-    Task AddAsync(Actor actor);
-    Task<Actor> UpdateAsync(int id, Actor newActor);
-    Task DeleteAsync(int id);
-}
+public interface IActorsService:IEntityBaseRepository<Actor>
+{}
